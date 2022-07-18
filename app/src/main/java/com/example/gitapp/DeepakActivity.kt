2b140b05fdcb73b249c269f63067ef89d5386675
DeepakAmane <deepaksubamane@gmail.com>
2022-07-18 16:44:45 +0530

    MaSter ChangeS done (MaSter 1) Added DeepakActivity.kt file and reSource

---
+++ app/src/main/java/com/example/gitapp/DeepakActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gitapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DeepakActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deepak)
+    }
+}
